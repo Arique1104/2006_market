1414ea5231c214d55d362050a1762f1e427f75d3
Arique Aguilar <arique1104@gmail.com>
2020-07-29 11:04:35 -0500

    Creates total_inventory method for market class

--- lib/market.rb
+++ lib/market.rb
@@ -23,12 +23,24 @@ class Market
     end
   end
 
+  def total_inventory
+    inventory_hash = Hash.new { |h,k| h[k] = {quantity: 0, vendors: []}}
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, amount|
+        inventory_hash[item][:quantity] += amount
+        inventory_hash[item][:vendors] << vendor
+      end
+    end
+    inventory_hash
+    # require "pry"; binding.pry
+  end
+
 end
 
-# A Vendor will be able to calculate their `potential_revenue` - the sum of all their items' price * quantity.
+# Add a method to your `Market` class called `sorted_item_list` that returns a list of names of all items the Vendors have in stock, sorted alphabetically. This list should not include any duplicate items.
 #
-# A Market is responsible for keeping track of Vendors. It should have a method called `vendor_names` that returns an array of all the Vendor's names.
+# Additionally, your `Market` class should have a method called `total_inventory` that reports the quantities of all items sold at the market. Specifically, it should return a hash with items as keys and hash as values - this sub-hash should have two key/value pairs: quantity pointing to total inventory for that item and vendors pointing to an array of the vendors that sell that item.
 #
-# Additionally, the Market should have a method called `vendors_that_sell` that takes an argument of an item represented as a String. It will return a list of Vendors that have that item in stock.
+# You `Market` will also be able to identify `overstocked_items`.  An item is overstocked if it is sold by more than 1 vendor AND the total quantity is greater than 50.
 #
-# Use TDD to create a `Market` class that responds to the following interaction pattern:
+# Use TDD to update your `Market` class so that it responds to the following interaction pattern:
