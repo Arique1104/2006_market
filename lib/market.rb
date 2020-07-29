5297af78add6203aa7d662723eb7daa3a1f52a19
Arique Aguilar <arique1104@gmail.com>
2020-07-29 10:24:43 -0500

    Adds required files

---
+++ lib/market.rb
@@ -0,0 +1,7 @@
+# A Vendor will be able to calculate their `potential_revenue` - the sum of all their items' price * quantity.
+#
+# A Market is responsible for keeping track of Vendors. It should have a method called `vendor_names` that returns an array of all the Vendor's names.
+#
+# Additionally, the Market should have a method called `vendors_that_sell` that takes an argument of an item represented as a String. It will return a list of Vendors that have that item in stock.
+#
+# Use TDD to create a `Market` class that responds to the following interaction pattern:
---
+++ test/market_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/item"
+require "./lib/vendor"
+require "./lib/market"
+
+class MarketTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
